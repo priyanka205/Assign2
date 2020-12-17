7878313bef55c35bf6a7fd280d19d128d8ec39a6
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-12-17 17:30:56 -0500

    answer aded

--- Assignment02/sub_process.py
+++ Assignment02/sub_process.py
@@ -1,4 +1,13 @@
 import subprocess
 
-with open('./newtasklist01.txt','w+') as fout:
-    subprocess.run("tasklist",stdout=fout)
+# with open('./newtasklist01.txt','w+') as fout:
+with open('./priyanka02.txt','w+') as fout:
+    subprocess.run("tasklist",stdout=fout) #to create new file
+
+
+    # to run the windows subprocess command
+    #in terminal: python sub_process.py or tasklist > priyanka02.txt
+
+with open('./priyanka02.txt','r') as infile:
+    for line in infile:
+        print(line) #to read file
