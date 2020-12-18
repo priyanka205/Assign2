f4eb9ffa6d8c9e6670f339a2e3734768c257af17
priyanka205 <63328436+priyanka205@users.noreply.github.com>
2020-12-18 15:48:46 -0500

    added

--- Assignment02/sub_process.py
+++ Assignment02/sub_process.py
@@ -5,9 +5,21 @@ with open('./priyanka02.txt','w+') as fout:
     subprocess.run("tasklist",stdout=fout) #to create new file
 
 
-    # to run the windows subprocess command
-    #in terminal: python sub_process.py or tasklist > priyanka02.txt
+#     # to run the windows subprocess command
+#     #in terminal: python sub_process.py or tasklist > priyanka02.txt
 
 with open('./priyanka02.txt','r') as infile:
     for line in infile:
-        print(line) #to read file
+        print(line) #to read file.
+
+# from this we can create any file from terminal form python command
+
+import subprocess
+import sys 
+
+filename = sys.argv[1]
+
+with open(f'./{filename}.txt','w+') as fout:
+    subprocess.run("tasklist",stdout=fout)
+
+#in terminal we nedd to write python sub_process and name for the txt file eg: mylist01.txt
